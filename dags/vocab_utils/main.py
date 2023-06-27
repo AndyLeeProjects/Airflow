014794bba67610b9154b278a1a9b042de48f849a
AndyLeeProjects <anddy0622@gmail.com>
2023-06-26 21:19:19 -0400

    fix split

--- dags/vocab_utils/main.py
+++ dags/vocab_utils/main.py
@@ -85,8 +85,8 @@ class LearnVocab():
 
         # Check if the vocab is already in the database
         for vocab in added_vocabs:
-            vocab = vocab.split(" (")[0]
-            if len(vocab.split(' ')) < 6:
+            vocab_len = vocab.split("(")[0].strip()
+            if len(vocab_len.split(' ')) < 6:
                 
                 # Append "vocab_origin" (e.g. tree (I like tree))
                 if "(" in vocab and ")" in vocab:
